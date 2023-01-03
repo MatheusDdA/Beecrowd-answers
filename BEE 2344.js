c37f65a9626bd2ba6c3f5530c173c1c74c990489
GOLDEM <mdouradoazevedo2018@gmail.com>
2023-01-03 16:51:00 -0300

    Notas da prova

---
+++ BEE 2344.js
@@ -0,0 +1,20 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var nota = parseInt(lines.shift());
+
+if (nota <= 0){
+    console.log(`E`);
+}
+else if (nota >= 1 && nota <= 35) {
+    console.log(`D`);
+}
+else if (nota >= 36 && nota <= 60) {
+    console.log(`C`);
+}
+else if (nota >= 61 && nota <= 85) {
+    console.log(`B`);
+}
+else if (nota >= 86 && nota <= 100) {
+    console.log(`A`);
+}
